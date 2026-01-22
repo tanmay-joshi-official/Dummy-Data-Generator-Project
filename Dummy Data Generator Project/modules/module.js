5cc4657e14466a45476cab988ec8869a61e05c71
Tanmay Joshi <tanmayjoshi579@gmail.com>
2026-01-22 19:44:15 +0530

    Delete Dummy Data Generator Project directory

--- Dummy Data Generator Project/modules/module.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose"
-
-let data = new mongoose.Schema({"Name": String, "Salary": Number, "Language": String, "City": String, "isManager": Boolean})
-
-export const employee = mongoose.model('employee', data)
--- Dummy Data Generator Project/public/script.js
+++
@@ -1,4 +0,0 @@
-button.addEventListener("click", async () => {
-    await fetch("http://localhost:3000/", {method: "POST"})
-    alert("10 Employees data successfully generated and previous record deleted!")
-})
